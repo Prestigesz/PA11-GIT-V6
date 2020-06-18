afa8abd411772fca524dde19ef8360126524f826
kaixin02 <64114925+Prestigesz@users.noreply.github.com>
2020-06-18 15:36:23 +0800

    Updated by Kaixin

---
+++ 02_kaixin_PA11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text Txt_Score = null;
+    public static GameManager thisManager = null;
+    private int Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateScore()
+    {
+        Score++;
+        Txt_Score.text = "SCORE : " + Score;
+    }
+    private void StartGame()
+    {
+        Score = 0;
+        Txt_Score.text = "SCORE : 0";
+    }
+}
---
+++ 02_kaixin_PA11/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] SpawnObject;
+    float PositionY;
+    private int SpawnPoints;
+    private float spawntime = 1f;
+    private float spawndelay = 2f;
+    private int randomInt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", spawntime, spawndelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        randomInt = Random.Range(0, SpawnObject.Length);
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject[randomInt], transform.position, transform.rotation);
+
+        //Instantiate(SpawnObject, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+    }
+}
